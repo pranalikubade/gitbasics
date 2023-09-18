786bd52a7bae0e8d8652d0cdb0c543e7e4c9635a
pranali <pranalikubade2000@gmail.com>
2023-09-18 16:03:37 +0530

    [Pranali:Add JavaScript Function Problems JS Prompt Added]

---
+++ JS_Prompt/While_loop_Problems/Flip_Coin.js
@@ -0,0 +1,26 @@
+
+// let headsCount = 0;
+// let tailsCount = 0;
+
+// while (headsCount < 11 && tailsCount < 11) {
+  
+//   const coinFlip = Math.floor(Math.random() * 2);
+
+//   if (coinFlip == 0) {
+//     headsCount++;
+//     console.log("Heads");
+//   }
+  
+//   else {
+//     tailsCount++;
+//     console.log("Tails");
+//   }
+// }
+const prompt = require('prompt-sync')();
+var num=prompt('Enter a number');
+console.log(num);
+// if (headsCount == 11) {
+//   console.log("Heads wins!");
+// } else {
+//   console.log("Tails wins!");
+// }
